f25f12d21dedaf5e6218e15c1cb0b81a26f0ed7d
vdtkk <vdtkk@hotmail.com>
2023-04-30 00:00:40 +0300

    MSSQL'den Python ile veri çekme

---
+++ main.py
@@ -0,0 +1,29 @@
+import pyodbc
+import urllib
+import pandas as pd
+from openpyxl.utils.dataframe import dataframe_to_rows
+from sqlalchemy import create_engine 
+# SQL Server'dan verileri alma
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-G1HC1O3\TEST;'
+                      'Database=turko;'
+                      'Trusted_Connection=yes;')
+
+# SQLAlchemy bağlantısı oluşturma
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(
+    urllib.parse.quote_plus('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-G1HC1O3\TEST;DATABASE=turko;Trusted_Connection=yes;')
+)
+# cursor = conn.cursor()
+# sql = "EXEC spDonemIciSatisButun @baslangic = ?, @bitis = ?,@depoInds = ?"
+# cursor.execute(sql, ('2023.01.01', '2023.01.31','4'))
+# sonuc = cursor.fetchall()
+# print(sonuc)
+
+engine = create_engine(conn_str)
+query = "SELECT * FROM Stoklar"
+df = pd.read_sql(query, engine)
+
+
+writer = pd.ExcelWriter('D:\python\stoklar.xlsx')
+df.to_excel(writer, index=False)
+writer._save()
